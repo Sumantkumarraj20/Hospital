e618b511febb1022c0e3c7d69d471fe37c960b04
sumantkumarraj20 <sumantkumarraj20@gmail.com>
2023-03-28 11:01:53 +0300

    initial commit

--- src/components/Card/Card.jsx
+++
@@ -1,8 +0,0 @@
-import style from "./Card.module.css";
-import React, { useEffect } from "react";
-
-const Card = ({children }) => {
-  return <div className={style.card}>{children}</div>;
-};
-
-export default (Card);
